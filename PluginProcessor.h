d17c98eb174e516da9ee3ffc3b96380c8482d098
cryptologicpsy <101523970+cryptologicpsy@users.noreply.github.com>
2023-06-05 23:32:40 +0300

    Add files via upload

---
+++ PluginProcessor.h
@@ -0,0 +1,97 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#define MAX_DELAY_TIME 2
+
+//==============================================================================
+/**
+*/
+class FlangerPluginAudioProcessor  : public juce::AudioProcessor
+                            #if JucePlugin_Enable_ARA
+                             , public juce::AudioProcessorARAExtension
+                            #endif
+{
+public:
+    //==============================================================================
+    FlangerPluginAudioProcessor();
+    ~FlangerPluginAudioProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    float FlangerPluginAudioProcessor::lin_interp(float sample_x, float sample_x1, float inPhase);
+
+
+private:
+    
+    
+
+    juce::AudioParameterFloat* mDryWetParameter;
+    
+    
+    
+   
+    juce::AudioParameterFloat* mDepthParameter;
+
+    juce::AudioParameterFloat* mRateParameter;
+
+    juce::AudioParameterFloat* mPhaseOffsetParameter;
+
+    juce::AudioParameterFloat* mFeedbackParameter;
+
+    juce::AudioParameterInt* mTypeParameter;
+
+    
+
+    float* mCircularBufferLeft;
+    float* mCircularBufferRight;
+
+    int mCircularBufferWriteHead;
+    int mCirqularBufferLength;
+
+    float mFeedbackLeft;
+    float mFeedbackRight;
+
+    float mLFOPhase;
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FlangerPluginAudioProcessor)
+};
